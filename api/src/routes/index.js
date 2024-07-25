2d5831fc92756c68c4b5289da7476ca570b99c83
Carlos Perales <carlosperales050@gmail.com>
2024-07-24 19:24:54 -0600

    add : books  router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const homeRouter = require("./homeRouter");
+const booksRouter = require("../routes/books.router");
 
 function routerApi(app) {
   const router = express.Router();
 
   app.use("/api/v1", router);
   router.use("/home", homeRouter);
+  router.use("/books", booksRouter);
 }
 module.exports = routerApi;
